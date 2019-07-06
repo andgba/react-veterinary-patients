fde3830953c10443cc6d2b71e6f36b2f62da4559
Andrés Gas <andresgas4@gmail.com>
2019-07-06 17:53:36 +0200

    Se crea componente formulario sin acción

---
+++ src/components/NuevaCita.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import Header from "./Header";
+
+class NuevaCita extends Component {
+    state = {}
+    render() {
+        return (
+            <div className="card mt-5 py-5">
+                <div className="card-body">
+                    <h2 className="card-title text-center mb-5">Crear nueva cita</h2>
+
+                    <form>
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Nombre Mascota</label>
+                            <div className="col-sm-8 col-lg-10">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre de la mascota"
+                                    name="mascota"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Nombre Dueño</label>
+                            <div className="col-sm-8 col-lg-10">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre del dueño de la mascota"
+                                    name="propietario"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Fecha</label>
+                            <div className="col-sm-8 col-lg-4">
+                                <input
+                                    type="date"
+                                    className="form-control"
+                                    name="fecha"
+                                />
+                            </div>
+
+                            <label className="col-sm-4 col-lg-2 col-form-label">Hora</label>
+                            <div className="col-sm-8 col-lg-4">
+                                <input
+                                    type="time"
+                                    className="form-control"
+                                    name="hora"
+                                />
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-sm-4 col-lg-2 col-form-label">Sintomas</label>
+                            <div className="col-sm-8 col-lg-10">
+                                <textarea
+                                    className="form-control"
+                                    name="sintomas"
+                                    placeholder="Describe los sintomas de tu mascota"></textarea>
+                            </div>
+                        </div>
+
+                        <input
+                            type="submit"
+                            className="py-3 mt-2 btn btn-success btn-block"
+                            value="Insertar nueva cita"/>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default NuevaCita;
